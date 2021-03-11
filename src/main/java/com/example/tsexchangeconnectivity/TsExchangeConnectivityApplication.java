5b0f0b6b54cd71936d09a45e1ea40be9a8b3fdec
penbosso <ckbosso@st.ug.edu.gh>
2021-03-11 15:44:03 +0000

    initial setup

---
+++ src/main/java/com/example/tsexchangeconnectivity/TsExchangeConnectivityApplication.java
@@ -0,0 +1,13 @@
+package com.example.tsexchangeconnectivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsExchangeConnectivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsExchangeConnectivityApplication.class, args);
+	}
+
+}
